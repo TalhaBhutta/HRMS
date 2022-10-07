c4e02652509a726d01b85c524a35c269a3c13421
TalhaBhutta <63284502+TalhaBhutta@users.noreply.github.com>
2022-10-08 03:03:12 +0500

    Migration for Changing Day column in db
    Migration for Changing Day column in db

---
+++ Migrations/20221007220232_Day1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HRMS.Migrations
+{
+    public partial class Day1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Day",
+                table: "Attendance",
+                type: "datetimeoffset",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Day",
+                table: "Attendance",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldNullable: true);
+        }
+    }
+}
